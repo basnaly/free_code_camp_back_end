24d7f63537d6ebe32417d1c60c96a620b2afc41f
Nataly <basnaly@gmail.com>
2022-06-17 16:27:22 +0300

    add error handling  + today

--- api/Timestamp/timestamp.js
+++ api/Timestamp/timestamp.js
@@ -15,17 +15,43 @@ router.get("/", function (req, res) {
 
 router.get('/api/:date', function (req, res) { // date: 2022-06-16
     let date = new Date(req.params.date) // try to create date.object 
+
     if (isNaN(date.getTime())) { // if data is invalid in was timestamp
       date = new Date(+req.params.date) // transform to number and create date.object
     }
+    console.log(date)
+    
     let GMTdate = date.toGMTString();
       console.log(GMTdate)  // "utc": "Thu, 16 Jun 2022 00:00:00 GMT"
+
+    if (GMTdate === 'Invalid Date'){
+      res.json({
+        "error": 'Invalid Date'
+      });
+      return
+    }
+    
     let timestamp = date.getTime()
       console.log(timestamp) // "unix": 1655337600000,
+
     res.json({
       "unix": timestamp,
       "utc": GMTdate
     })
   })
+
+router.get('/api/', function(req, res) {
+  let date = new Date();
+
+  let GMTdate = date.toGMTString();
+    console.log(GMTdate)
+
+  let timestamp = date.getTime()
+    
+  res.json({
+    "unix": timestamp,
+    "utc": GMTdate
+  })
+})
 
 module.exports = router
